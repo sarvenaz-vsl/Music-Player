355eb6d0c8b7f80ec632ed8c74dcd69c1b3df83c
sarvenaz-vsl <sarvenaz.vsl@gmail.com>
2021-05-07 14:31:44 +0430

    adding button to music list

--- script.js
+++ script.js
@@ -9,24 +9,24 @@ checkbox.addEventListener('change', function() {
 })
 
 // Music
-let dir = "songs/";
 
-let playlist = ["Cartoon - On & On" , "Diviners-X-Riell-Slow" , "InfiNoise-Sunlight","Jone - Everything",
-"Last Heroes x TwoWorldsApart - Eclipse","Lost Sky - Fearless pt.II"]
+let playlist = ["music-1", "music-2", "music-3", "music-4", "music-5", "music-6"];
 
 let title = ["Cartoon - On & On" , "Slow" , "Sunlight","Everything","Last Heroes",
-"Fearless"]
+"Fearless"];
+
+let artists = ["Daniel Levi", "Diviners-X-Riell","Nilka","Jone","AERYN","Chris Linton"];
+
 let poster = ["img/1.jpg","img/2.jpg","img/3.jpg","img/4.jpg","img/5.jpg","img/6.jpg"]
-let artists = ["Daniel Levi", "Diviners-X-Riell","Nilka","Jone","AERYN","Chris Linton"]
 
 playlist_index = 0;
+
+let dir = "songs/";
 
 let ext = ".mp3";
 
 let agent = navigator.userAgent.toLowerCase();
-if(agent.indexOf('firefox') != -1 || agent.indexOf('opera') != -1) {
-    ext = ".ogg";
-}
+if(agent.indexOf('firefox') != -1 || agent.indexOf('opera') != -1) { ext = ".ogg"; }
 
 let playBtn = document.getElementById("playPauseBtn");
 let prevBtn = document.getElementById("prevBtn");
@@ -47,11 +47,10 @@ var audio = new Audio();
 audio.src = dir + playlist[0] + ext;
 audio.loop = false;
 
+playlist_status.innerHTML = title[playlist_index];
+playlist_artist.innerHTML = artists[playlist_index];
 
-window.addEventListener('load', () => { 
-    loadMusic(playlist_index); 
-    playingNow();
-});
+window.addEventListener('load', () => { playingNow(); });
 playBtn.addEventListener('click', playPause);
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
@@ -73,11 +72,8 @@ progress.addEventListener('click', (e) => {
     }
 });
 
+
 // Functions
-function loadMusic() {
-    playlist_status.innerHTML = title[playlist_index];
-    playlist_artist.innerHTML = artists[playlist_index];
-}
 
 function fetchMusicDetail() {
     $("#image").attr("src", poster[playlist_index]);
@@ -103,6 +99,7 @@ function random() {
     playlist_index = randomIndex;
     fetchMusicDetail();
     document.querySelector(".playPause").classList.add("active");
+    playingNow();
 }
 
 function loop() {
@@ -113,6 +110,7 @@ function loop() {
         audio.loop = true;
         document.querySelector(".loop").classList.add("active");
     }
+    playingNow();
 }
 
 function nextSong() {
@@ -122,6 +120,7 @@ function nextSong() {
         playlist_index = 0;
     }
     fetchMusicDetail();
+    playingNow();
 }
 
 function prevSong() {
@@ -131,6 +130,7 @@ function prevSong() {
         playlist_index = playlist.length - 1;
     }
     fetchMusicDetail();
+    playingNow();
 }
 
 function playPause() {
@@ -143,6 +143,7 @@ function playPause() {
         document.querySelector(".playPause").classList.remove("active");
         document.querySelector(".image").classList.remove("play");
     }
+    playingNow();
 }
 
 function switchTrack() {
@@ -152,6 +153,7 @@ function switchTrack() {
         playlist_index++;
     }
     fetchMusicDetail();
+    playingNow();
 }
 
 function seek(e) {
@@ -178,27 +180,36 @@ function seekTimeUpdate() {
         durationTimeText.innerHTML = "00" + ":" + "00";
     }
 }
+
 const ulTag = document.querySelector("ul");
 for(i = 0; i < playlist.length; i++) {
     
-    let liTag = `<li li-index="${i}" li-name="${artists[i]}">
+    let liTag = `<li li-index="${i}"}">
                     <div class="row">
                         <span>${title[i]}</span>
                         <p>${artists[i]}</p>
                     </div>
                     
+                    <span class="audio-duration">
+                        <div class="button-lg-list">
+                            <i class="fas fa-play-circle fa-lg"></i>
+                            <i class="fas fa-pause-circle fa-lg"></i>
+                        </div>
+                    </span>
                 </li>`;
-    ulTag.insertAdjacentHTML("beforeend", liTag);
 
-   
+    ulTag.insertAdjacentHTML("beforeend", liTag);
 }
 
 const allLiTags = ulTag.querySelectorAll("li");
 function playingNow() {
     for(j = 0; j < allLiTags.length; j++) {
-        if(allLiTags[j].getAttribute("li-name") == playlist_index) {
+        if(allLiTags[j].classList.contains("playing")) { allLiTags[j].classList.remove("playing"); }
+
+        if(allLiTags[j].getAttribute("li-index") == playlist_index) { 
             allLiTags[j].classList.add("playing");
-        }
+        } 
+        
         allLiTags[j].setAttribute("onclick", "clicked(this)")
     }
 }
@@ -206,7 +217,6 @@ function playingNow() {
 function clicked(element) {
     let getLiIndex = element.getAttribute("li-index");
     playlist_index = getLiIndex;
-    loadMusic(playlist_index);
     fetchMusicDetail();
     playPause();
     if(audio.paused) {
@@ -215,5 +225,4 @@ function clicked(element) {
         document.querySelector(".image").classList.add("play");
     }
     playingNow();
-    showMoreBtn.click();
 }
